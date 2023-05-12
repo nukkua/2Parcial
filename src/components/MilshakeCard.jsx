3f7fc84aacf75db4f9b7b1326273aab73e02dc3b
nukkua <marco.salazar.2014.com@gmail.com>
2023-05-12 14:30:26 -0400

    primer componente

---
+++ src/components/MilshakeCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './MilshakeCard.css'
+
+export const MilshakeCard = (props) => {
+    const milshake=props.milshake
+  return (
+    <div className="milshake-card">
+      <h3>{milshake.name}</h3>
+      <p>{milshake.description}</p>
+      <span>{milshake.price}</span>
+    </div>
+  );
+};
